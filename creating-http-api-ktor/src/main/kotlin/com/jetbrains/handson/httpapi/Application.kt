6546fb88f6584795d5908a4fd2104544dfb9eddb
centos <centos@localhost.localdomain>
2020-11-04 00:08:12 -0500

    add import to fix compile error

--- creating-http-api-ktor/src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ creating-http-api-ktor/src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,14 @@
 package com.jetbrains.handson.httpapi
 
 import io.ktor.application.Application
-
+import io.ktor.features.ContentNegotiation
+import io.ktor.application.install
+import io.ktor.serialization.json
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    install(ContentNegotiation) {
+        json()
+    }
 }
