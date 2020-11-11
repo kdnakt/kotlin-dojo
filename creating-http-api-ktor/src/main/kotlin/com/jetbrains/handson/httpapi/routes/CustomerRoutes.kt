67755b4a64f75bc001ac141a5b50a91f2a70c748
GOAMI_Takaaki <goami@works-hi.co.jp>
2020-11-11 16:14:45 +0900

    add get(id)/post/delete(id) implementation and routing

--- creating-http-api-ktor/src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
+++ creating-http-api-ktor/src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -1,11 +1,19 @@
 package com.jetbrains.handson.httpapi.routes
 
+import com.jetbrains.handson.httpapi.models.Customer
 import com.jetbrains.handson.httpapi.models.customerStorage
-import io.ktor.application.call
+import io.ktor.application.*
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.*
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.*
+
+fun Application.registerCustomerRoutes() {
+    routing {
+        customerRouting()
+    }
+}
 
 fun Route.customerRouting() {
     route("/customer") {
@@ -17,13 +25,29 @@ fun Route.customerRouting() {
             }
         }
         get("{id}") {
-
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                    "Missing or malformed id",
+                    status = HttpStatusCode.BadRequest
+            )
+            val customer =
+                    customerStorage.find { it.id == id } ?: return@get call.respondText(
+                            "No customer with id $id",
+                            status = HttpStatusCode.NotFound
+                    )
+            call.respond(customer)
         }
         post {
-
+            val customer = call.receive<Customer>()
+            customerStorage.add(customer)
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Accepted)
         }
         delete("{id}") {
-
+            val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
+            if (customerStorage.removeIf { it.id == id }) {
+                call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
+            } else {
+                call.respondText("Not Found", status = HttpStatusCode.NotFound)
+            }
         }
     }
 }
