7acd7d15155eae30eddb7f26a209134189926b7c
asada_n <asada_n@works-hi.co.jp>
2020-11-18 14:06:08 +0900

    ref the Order routes section and Manually testing HTTP endpoints
        * add get handlers on order and routings to it
        * add tests on customer endpoints and order endpoints

---
+++ creating-http-api-ktor/src/main/kotlin/com/jetbrains/handson/httpapi/models/Order.kt
@@ -0,0 +1,9 @@
+package com.jetbrains.handson.httpapi.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Order(val number: String, val contents: List<OrderItem>)
+
+@Serializable
+data class OrderItem(val item: String, val amount: Int, val price: Double)
---
+++ creating-http-api-ktor/src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
@@ -0,0 +1,64 @@
+package com.jetbrains.handson.httpapi.routes
+
+import com.jetbrains.handson.httpapi.models.Order
+import com.jetbrains.handson.httpapi.models.OrderItem
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+
+fun Route.listOrdersRoute() {
+    get("/order") {
+        if (orderStorage.isNotEmpty()) {
+            call.respond(orderStorage)
+        }
+    }
+}
+
+fun Route.getOrderRoute() {
+    get("/order/{id}") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        call.respond(order)
+    }
+}
+
+fun Route.totalizeOrderRoute() {
+    get("/order/{id}/total") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        val total = order.contents.map { it.price * it.amount }.sum()
+        call.respond(total)
+    }
+}
+
+fun Application.registerOrderRoutes() {
+    routing {
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+    }
+}
+
+val orderStorage = listOf(
+    Order(
+    "2020-04-06-01", listOf(
+        OrderItem("Ham Sandwich", 2, 5.50),
+        OrderItem("Water", 1, 1.50),
+        OrderItem("Beer", 3, 2.30),
+        OrderItem("Cheesecake", 1, 3.75)
+    )),
+    Order("2020-04-03-01", listOf(
+        OrderItem("Cheeseburger", 1, 8.50),
+        OrderItem("Water", 2, 1.50),
+        OrderItem("Coke", 2, 1.76),
+        OrderItem("Ice Cream", 1, 2.35)
+    ))
+)
