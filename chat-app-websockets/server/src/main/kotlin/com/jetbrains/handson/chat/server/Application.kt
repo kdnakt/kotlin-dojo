bde593177c8639ce88f8232c28495508d38a3efd
GOAMI_Takaaki <goami@works-hi.co.jp>
2020-12-16 14:12:57 +0900

    changes of kotlin-dojo on Dec.16th,2020

--- chat-app-websockets/server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
+++ chat-app-websockets/server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
@@ -13,13 +13,26 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
     install(WebSockets)
     routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
         webSocket("/chat") {
-            send("You are connected!")
-            for(frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receivedText = frame.readText()
-                println("received $receivedText")
-                send("You said: $receivedText")
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUsername)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
             }
         }
     }
