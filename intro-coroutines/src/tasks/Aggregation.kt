f1753de4216361374a99990e036fba87bb0907d5
kdnakt <a.kid.1985@gmail.com>
2021-02-03 14:26:35 +0900

    imlement sort

--- intro-coroutines/src/tasks/Aggregation.kt
+++ intro-coroutines/src/tasks/Aggregation.kt
@@ -15,4 +15,4 @@ TODO: Write aggregation code.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
 fun List<User>.aggregate(): List<User> =
-    this
+    this.sortedByDescending { it.contributions }
